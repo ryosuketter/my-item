cd4ded8819b622e60782388edcfc522318a4a79b
ryosuketter <r.shimizu.yale@gmail.com>
2025-07-24 02:40:10 +0900

    Refactor GET request handler in product API to improve parameter typing and destructuring

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 import { getProduct } from "@/lib/microcms";
 
-export async function GET(request, { params }) {
-  const { id } = params;
+export async function GET(_: Request, { params }: { params: { id: string } }) {
+  const { id } = await params;
   const res = await getProduct(id);
   return NextResponse.json(res);
 }
